0384f221986c40cfaf91daf6931a990e419345d0
nishuldhakar <nishuldhakar.se@gmail.com>
2026-01-22 12:37:44 +0530

    Fix CORS and crash on Render

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
 
 app.use(morgan('dev'));
 app.use(express.json());
